a29d85db1780778005d2609de13c04a512170ba9
Michi Hirczy <mi.hi@gmx.net>
2025-08-03 17:44:21 +0200

    working datareader!!!

--- src/lib/BMSReader.cpp
+++ src/lib/BMSReader.cpp
@@ -1,14 +1,48 @@
 #include "BMSReader.h"
 
 BMSReader::BMSReader(void) {
+    
+
+
     std::cout << "BMSReader created\n";
 }
 
 BMSReader::~BMSReader(void) {
+    if (gSharedMemPtr) {
+        UnmapViewOfFile(gSharedMemPtr);
+        gSharedMemPtr = NULL;
+    }
+    if (gSharedMemPtr2) {
+        UnmapViewOfFile(gSharedMemPtr2);
+        gSharedMemPtr2 = NULL;
+    }
+    CloseHandle(gSharedMemHandle);
+    CloseHandle(gSharedMemHandle2);
     std::cout << "BMSReader deleted\n";
 }
 
-void  BMSReader::readF16Data(F16Data * data) {
-    std::cout << "Reading from BMS!\n";
-    data->cabinPress = 5000;
+bool BMSReader::connectToSim() {
+
+    if ((gSharedMemPtr) && (gSharedMemPtr2)) return true; // are pointers already mapped?
+
+    TCHAR dataName[] = TEXT("FalconSharedMemoryArea");
+    TCHAR data2Name[] = TEXT("FalconSharedMemoryArea2");
+    gSharedMemHandle = OpenFileMapping(FILE_MAP_READ, TRUE, dataName);
+    gSharedMemHandle2 = OpenFileMapping(FILE_MAP_READ, TRUE, data2Name);
+
+    if ((gSharedMemHandle) && (gSharedMemHandle2)) {  // BMS is running
+        gSharedMemPtr = MapViewOfFile(gSharedMemHandle, FILE_MAP_READ, 0, 0, 0);
+        gSharedMemPtr2 = MapViewOfFile(gSharedMemHandle2, FILE_MAP_READ, 0, 0, 0);
+        if ((gSharedMemPtr) && (gSharedMemPtr2)) return true;
+    }
+    return false;
+}
+
+void  BMSReader::readF16Data(F16Data* data) {
+    // std::cout << "Reading from BMS!\n";
+    flightData = (FlightData*)gSharedMemPtr;
+    flightData2 = (FlightData2*)gSharedMemPtr2;
+
+    data->fuelFWD = (long)flightData->fwd;
+    std::cout << "\r" << flightData->fwd;
 }
--- src/lib/BMSReader.h
+++ src/lib/BMSReader.h
@@ -1,12 +1,27 @@
 #pragma once
 #include "DataReader.h"
 #include "FlightData.h"
+#include <stdio.h>
+#include <Windows.h>
 
 class BMSReader :
     public DataReader
 {
 public:
     void readF16Data(F16Data * data);
+    bool connectToSim();
     BMSReader(); // default constructor
     ~BMSReader(); // default constructor
+
+private:
+    FlightData* flightData;
+    FlightData2* flightData2;
+
+    HANDLE gSharedMemHandle = NULL;
+    void* gSharedMemPtr = NULL;
+    HANDLE gSharedMemHandle2 = NULL;
+    void* gSharedMemPtr2 = NULL;
+
+    
+
 };
