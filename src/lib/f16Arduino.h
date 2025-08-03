7ac719df741296696863040e4463100222351807
Michi Hirczy <mi.hi@gmx.net>
2025-08-03 21:36:54 +0200

    rearranged lightbits according to my hardware

--- src/lib/f16Arduino.h
+++ src/lib/f16Arduino.h
@@ -14,41 +14,38 @@
 // different order because of soldering on hardware
 // set those to "original" values - topleft to bottomright - if necessary
 
-#define CPSEAT      pow(2,0)
-#define CPNWS       pow(2,1)  
-#define CPANTISKID  pow(2,2)
-#define CPHOOK      pow(2,3)
-#define CPAVIONICS  pow(2,4)
-#define CPEQUIPHOT  pow(2,5)
-#define CPRADARALT  pow(2,6)
-#define CPIFF       pow(2,7)
-#define CPNUCLEAR   pow(2,8)
-#define CPENGINE    pow(2,9)
-#define CPSEC       pow(2,10)
-#define CPFUELOIL   pow(2,11)
-#define CPINLET     pow(2,12)
-#define CPOVERHEAT  pow(2,13)
-#define CPFLCS      pow(2,14)
-#define CPELECSYS   pow(2,15)
-#define CPPROBEHEAT pow(2,16)
-#define CPCADC      pow(2,17)
-#define CPAFTFUEL   pow(2,18)
-#define CPFWDFUEL   pow(2,19)
-#define CPATF       pow(2,20)
-#define CPSTORESCFG pow(2,21)
-#define CPLINE1     pow(2,22)
-#define CPBUC       pow(2,23)
-#define CPEEC       pow(2,24)
-#define CPOVERHEAT  pow(2,25)
-#define CPLINE2     pow(2,26)
-#define CPLINE3     pow(2,27)
-#define CPLINE4     pow(2,28)
-#define CPLINE5     pow(2,29)
-#define CPLINE6     pow(2,30)
-#define CPCABIN     pow(2,31)
-
-
-
+#define CPSEAT      0x00000001
+#define CPNWS       0x00000002 
+#define CPANTISKID  0x00000004
+#define CPHOOK      0x00000008
+#define CPAVIONICS  0x00000010
+#define CPEQUIPHOT  0x00000020
+#define CPRADARALT  0x00000040
+#define CPIFF       0x00000080
+#define CPENGINE    0x00000100
+#define CPSEC       0x00000200
+#define CPFUELOIL   0x00000400
+#define CPINLET     0x00000800
+#define CPFLCS      0x00001000
+#define CPELECSYS   0x00002000
+#define CPPROBEHEAT 0x00004000
+#define CPCADC      0x00008000
+#define CPAFTFUEL   0x00010000
+#define CPFWDFUEL   0x00020000
+#define CPATF       0x00040000
+#define CPSTORESCFG 0x00080000
+#define CPLINE1     0x00100000
+#define CPBUC       0x00200000
+#define CPEEC       0x00400000
+#define CPOVERHEAT  0x00800000
+#define CPLINE2     0x01000000
+#define CPLINE3     0x02000000
+#define CPLINE4     0x04000000
+#define CPNUCLEAR   0x08000000
+#define CPLINE5     0x10000000
+#define CPLINE6     0x20000000
+#define CPCABIN     0x40000000
+#define CPOXYLOW    0x80000000
 
 // variable numbers to use to configure data to get from F16Data.h
 #define POWERSTATES         1
