bef721ef014fb8d5ca5a9fc8a8e9eca3407a8cce
Michi Hirczy <mi.hi@gmx.net>
2025-08-11 20:35:30 +0200

    Update Controller.h

--- src/lib/Controller.h
+++ src/lib/Controller.h
@@ -9,11 +9,13 @@ class Controller
 {
 public:
     Controller(std::string name, std::string comPort, long baudrate, std::vector<unsigned char> datafieldsIn);
+    Controller(std::string name, std::string comPort, long baudrate);
     Controller();
     ~Controller();
     std::string getName();
     
     void setDataField(int pos, int value);
+    void addDataField(unsigned char value);
     std::vector<unsigned char> getDatafields();
 
     bool connect();
@@ -29,10 +31,13 @@ private:
     std::string controllerName;
     std::vector<unsigned char> datafields;
 
+    std::vector<unsigned char> splitValue(int value, int size);
+
+    void buildVarStringBegin(int varNum, unsigned char size, std::vector<char>& updateString);
+    void buildVarStringEnd(std::vector<char>& updateString);
     void buildVarString(int varNum, unsigned char value, std::vector<char>& updateString);    
     void buildVarString(int varNum, unsigned short value, std::vector<char>& updateString);
-    void buildVarString(int varNum, unsigned int value, std::vector<char>& updateString);
-    std::vector<unsigned char> splitValue(int value, int size);
+    void buildVarString(int varNum, unsigned int value, std::vector<char>& updateString);    
 
     void addVarDataToUpdateString(int i, std::vector<char> &updateString, F16Data * data, F16Data * prevData);
     void addByteToUpdateString(std::vector<char>* updateString, char byte);
