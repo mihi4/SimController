76356fde48705b27f9e34a078244260d3b9114b7
Michi Hirczy <mi.hi@gmx.net>
2025-09-09 12:56:50 +0200

    Update Controller.cpp - removed calling debugUpdateString in updateController

--- src/classes/Controller.cpp
+++ src/classes/Controller.cpp
@@ -476,7 +476,7 @@ void Controller::updateController(F16Data* data, F16Data* prevData) {
         for (int i = 0; i < datafields.size(); i++) {
             updateString.clear();
             addVarDataToUpdateString(datafields[i], updateString, data, prevData);            
-            if (updateString.size() > 3) debugUpdateString(updateString);
+            // if (updateString.size() > 3) debugUpdateString(updateString);
             if (updateString.size() > 3) serialHandler.sendDataUpdate(updateString);
         }
 }
