bd72c7929f30fa24635338cbaf4ef44c07c56d20
mihi4 <mi.hi@gmx.net>
2025-09-14 11:49:26 +0200

    Update SC_UserConfig.h - wrong datatype for TotalFuel fixed

--- Arduino/SimController/SC_UserConfig.h
+++ Arduino/SimController/SC_UserConfig.h
@@ -62,7 +62,7 @@ int scNameSize = sizeof(scName)/sizeof(scName[0]);
 f16var* vars[] = { 
     new f16varC(SIMSTATES, MODNONE, 0, 0, 0)
   ,  new f16varC(POWERSTATES, MODNONE, 0, 0, 0)   // First 2 should always be configured in every module  
-  ,  new f16varI(FUELTOTAL, MODMAX7219, 0, 0, 0)
+  ,  new f16varC(FUELTOTAL, MODMAX7219, 0, 0, 0)
   ,  new f16varI(FUELFWD, MODSERVO, 2, 0, 0)
   ,  new f16varI(FUELAFT, MODSERVO, 3, 0, 0)
   /*,  new f16varI(HYDA, MODSERVO, 0, 0, 0)
