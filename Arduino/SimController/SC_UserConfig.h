59e592fd2db1e8690221d9e3ddd926be0003efeb
Michi Hirczy <mi.hi@gmx.net>
2026-08-03 08:39:27 +0200

    Update SC_UserConfig.h and SimController.ino

--- Arduino/SimController/SC_UserConfig.h
+++ Arduino/SimController/SC_UserConfig.h
@@ -17,29 +17,29 @@
   #endif
 
   //MODULE SELECTION - uncomment the modules you want to use.
-   
+
   //#define LED               //drive LEDs
   //#define LEDMatrix         //drive LED Matrix using a MAX7219 controller
   //#define LCD               //drive LCD display
-  #define LED_MM5451
-  #define SSegMAX7219       //drive 7-Segment displays via MAX7219 controller
+  //#define LED_MM5451
+  //#define SSegMAX7219       //drive 7-Segment displays via MAX7219 controller
   //#define SSegTM1637        //drive 7-Segment displays via TM1367 controller
   //#define SLx2016           //drive 4-digit 5x7 dotmatrix modules
-  #define ServoMotor        //drive servo motors directly connected to the arduino
+  //#define ServoMotor        //drive servo motors directly connected to the arduino
   //#define ServoPWM          //drive multiple servo motors via pwm shield
   //#define StepperBYJ        //drive stepper motor 28BYJ-48
-  #define StepperX27        //drive stepper motor X27.168
-  //#define StepperVID        //drive multiple stepper motors X25.168 with a VID66-06 controller
+  //#define StepperX27        //drive stepper motor X27.168
+  #define StepperVID        //drive multiple stepper motors X25.168 with a VID66-06 or AX1201728sg controller
   //#define CompassX27        //drive a compass with a Xxx.xxx -class stepper motor
   //#define AirCore           //drive multiple Air Core motors (via CS4192 controller chip)
   //#define MotorPoti         //motor-driven poti control
   //#define OLED              //display data on an OLED display
   //#define SpeedBrake        //Enable display of the SpeedBrake indicator on an 128x64 OLED display (DEDunino)
   //#define FuelFlowIndicator //Enable display of the FuelFlow indicator on an 128x64 OLED display (DEDunino)
-  //#define OLED_Compass      //Enable display of the wet compass on an 128x32 OLED display 
-  #define DED_PFL           //Enable display of DED or PFL on an 254x64 OLED display (DEDunino)
+  //#define OLED_Compass      //Enable display of the wet compass on an 128x32 OLED display
+  //#define DED_PFL           //Enable display of DED or PFL on an 254x64 OLED display (DEDunino)
   //#define Switches          //use the arduino to read switch positions and send keyboard commands
-  //#define MagSwitch         //enable magnetic held switches														   
+  //#define MagSwitch         //enable magnetic held switches
   //#define ButtonMatrix      //use the arduino to read switch positions and send keyboard commands
   //#define RotEncoder        //use the arduino to read rotary encoders and send keyboard commands
   //#define AnalogAxis        //use the arduino to read analog resistors and sync this with a gamecontroller axis
@@ -50,31 +50,31 @@
 
 //BASIC SETTINGS
 #define BAUDRATE 115200 // 57600 //    115200       // serial connection speed
-  
+
 static const char scName[] = "RightAux";
-int scNameSize = sizeof(scName)/sizeof(scName[0]);  
-  
+int scNameSize = sizeof(scName)/sizeof(scName[0]);
+
 //DATA VARIABLES
 /* var format:
   VarNumber, VarBytes, Module, index of modules (if more are available), Index of Value in module (depending on module), value
-  value index can be used for single bits in a led module or 255 to use the full value, for example, 
+  value index can be used for single bits in a led module or 255 to use the full value, for example,
 */
-f16var* vars[] = { 
+f16var* vars[] = {
     new f16varC(SIMSTATES, MODNONE, 0, 0, 0)
-  ,  new f16varC(POWERSTATES, MODNONE, 0, 0, 0)   // First 2 should always be configured in every module    
+  ,  new f16varC(POWERSTATES, MODNONE, 0, 0, 0)   // First 2 should always be configured in every module
   ,  new f16varI(HYDA, MODSERVO, 0, 0, 0)
-  ,  new f16varI(HYDB, MODSERVO, 1, 0, 0)  
+  ,  new f16varI(HYDB, MODSERVO, 1, 0, 0)
   ,  new f16varI(FUELFWD, MODSERVO, 2, 0, 0)
-  ,  new f16varI(FUELAFT, MODSERVO, 3, 0, 0)  
-  ,  new f16varC(FUELTOTAL, MODMAX7219, 0, 0, 0) 
-  ,  new f16varL(CAUTIONPANELLIGHTS, MODMM5451, 0, 255, 0)  
+  ,  new f16varI(FUELAFT, MODSERVO, 3, 0, 0)
+  ,  new f16varC(FUELTOTAL, MODMAX7219, 0, 0, 0)
+  ,  new f16varL(CAUTIONPANELLIGHTS, MODMM5451, 0, 255, 0)
   ,  new f16varS(PFDLINE1, MODDED, 255, 0, "                         " )
   ,  new f16varS(PFDLINE2, MODDED, 255, 0, "                         " )
   ,  new f16varS(PFDLINE3, MODDED, 255, 0, "                         " )
   ,  new f16varS(PFDLINE4, MODDED, 255, 0, "                         " )
   ,  new f16varS(PFDLINE5, MODDED, 255, 0, "                         " )
   ,  new f16varI(EPUFUEL, MODX27, 0, 0, 0)
-  ,  new f16varI(CABINPRESS, MODX27, 1, 0, 0) 
+  ,  new f16varI(CABINPRESS, MODX27, 1, 0, 0)
 };
 const char varCount = sizeof(vars)/sizeof(vars[0]);
 
@@ -83,7 +83,7 @@ const char varCount = sizeof(vars)/sizeof(vars[0]);
 
   // This is the most important part of this sketch. You need to set the data that the Arduino will have to handle
   // Make sure that you chose the definition of variableCount matches the number of entries in this table
-  // Fill data as follows: 
+  // Fill data as follows:
   // 1. Short description (max 5 characters)
   // 2. Flightdata ID (check BMSAIT Variablen.csv)  as a 4-digit string (needed for PULL mode)
   // 3. data type (check BMSAIT Variablen.csv: b=bool, y=byte, 1=byte[], i=integer, f=float, s=string, 2=string[]))  (needed for PULL mode)
@@ -91,24 +91,24 @@ const char varCount = sizeof(vars)/sizeof(vars[0]);
   // 5. Target - i.e. Output PIN of LEDs, a reference to an entry in a module specific data table (i.e. motors)
   // 6. Reference2 - i.e. the line on LCD displays or a link to an entry in container in motor modules
   // 7. Reference3 - i.e. number of characters to display (control display length on 7-segment or LCD displays)
-  // 8  Reference4 - i.e. start position (control position of data on 7-segment or LCD displays) 
+  // 8  Reference4 - i.e. start position (control position of data on 7-segment or LCD displays)
   // 9. Reference5 - i.e. decimal point (will add a decimal point on 7-segment displays after the given position)
   // 10. Initial value as string (i.e. "00")
- /** 
-  
+ /**
+
   Datenfeld datenfeld[]=
     {
       //Description ID    DT    OT    target Ref2 Ref3 Ref4 Ref5  RQ   IV
 	  {"In3D",   "1800",  'b',  99,   0,    0,   0,   0,   0,     "",  "F"}          //Variable 0 - Player is in 3D
-	  ,{"Fly",   "1478",  'b',  99,   0,    0,   0,   0,   0,     "",  "F"}		   //Variable 1 - Player is in Aircraft	  
+	  ,{"Fly",   "1478",  'b',  99,   0,    0,   0,   0,   0,     "",  "F"}		   //Variable 1 - Player is in Aircraft
 	 	  // HYDPRess Servos
 	  ,{"HydA",  "0281", 'i',  40,     0,   0,   0,   0,   0,     "",   0}	// 33 HYD PRESSURE A
 	  ,{"HydB",  "0291", 'i',  40,     1,   0,   0,   0,   0,     "",   0}	// 34 HYD PRESSURE B
 	  // FQTI Servos and max7219
       ,{"FWDFL", "0531",  'i',  40,   2,    0,   0,   0,   0,     "",   0}   // 35 FUEL QTY FWD
 	  ,{"AFTFL", "0481",  'i',  40,   3,    0,   0,   0,   0,     "",   0}	// 36 FUEL QTY AFT
-	  ,{"TOTFL", "0551",  's',	30,   0,    0,   5,   0,   0,     "",   0}	// 37 FUEL QTY TOTAL as String  
-// Caution panel lights MM5451 (13)	  
+	  ,{"TOTFL", "0551",  's',	30,   0,    0,   5,   0,   0,     "",   0}	// 37 FUEL QTY TOTAL as String
+// Caution panel lights MM5451 (13)
 	  ,{"ELEC",  "1591",  'b',  13,   0,   13,   0,   0,   0,     0,    0}  	// 7
 	  ,{"Probe", "1565",  'b',  13,   0,   14,   0,   0,   0,     0,    0} 		// 8 Probeheat
 	  ,{"Seat",  "1566",  'b',  99,   0,    0,   0,   0,   0,     0,    0} 		// 9 Seat Not Armed
@@ -122,7 +122,7 @@ const char varCount = sizeof(vars)/sizeof(vars[0]);
 	  ,{"Engn",  "1521",  'b',  99,   0,    8,   0,   0,   0,     0,    0}   	// 17 Engine Fault
 	  ,{"SEC",   "1563",  'b',  99,   0,    9,   0,   0,   0,     0,    0}		// 18
 	  ,{"Fuel",  "1568",  'b',  99,   0,   10,   0,   0,   0,     0,    0}  	// 19 Fuel Oil Hot
-	  ,{"Inlet", "1609",  'b',  99,   0,   11,   0,   0,   0,     0,    0} 		// 20 Inlet Icing 
+	  ,{"Inlet", "1609",  'b',  99,   0,   11,   0,   0,   0,     0,    0} 		// 20 Inlet Icing
 	  ,{"FLCS",  "1519",  'b',  99,   0,   12,   0,   0,   0,     0,    0}  	// 21 FLCS Fault
 	  ,{"CADC",  "1602",  'b',  99,   0,   15,   0,   0,   0,     0,    0}  	// 22
 	  ,{"AFuel", "1560",  'b',  99,   0,   17,   0,   0,   0,     0,    0} 		// 23 Aft Fuel low
@@ -135,18 +135,18 @@ const char varCount = sizeof(vars)/sizeof(vars[0]);
 	  ,{"OHeat", "1522",  'b',  99,   0,   24,   0,   0,   0,     0,    0} 		// 29 Overheat
 	  ,{"---2",  "1612", 'b',  99,     0,    25,   0,   0,  0,   0, 0}  // not in BMS
 	  ,{"---3",  "1612", 'b',  99,     0,    26,   0,   0,  0,   0, 0}  // not in BMS
-	  ,{"---4",  "1612", 'b',  99,     0,    27,   0,   0,  0,   0, 0}  // not in BMS 
+	  ,{"---4",  "1612", 'b',  99,     0,    27,   0,   0,  0,   0, 0}  // not in BMS
 	  ,{"Nucl",  "1610", 'b',  99,     0,  28,   0,   0,   0,     0,    0}  		// 30 not in BMS, uses BUC value from winapp
 	  ,{"---5",  "1612", 'b',  99,     0,    29,   0,   0,  0,   0, 0}  // not in BMS
-	  ,{"---6",  "1612", 'b',  99,     0,    30,   0,   0,  0,   0, 0}  // not in BMS 
+	  ,{"---6",  "1612", 'b',  99,     0,    30,   0,   0,  0,   0, 0}  // not in BMS
 	  ,{"CabPs", "1530", 'b',  99,     0,  31,   0,   0,   0,     0,    0}  // 31 CabinPressure
-	  ,{"OxLow", "1564", 'b',  99,     0,  32,   0,   0,   0,     0,    0}  // 32 OxyLow  
-	  ,{"CPAll", "1619", 'b',  13,     0,  0,   0,   0,   0,     0,    0}   // All Caution Lights on, index 7!	
+	  ,{"OxLow", "1564", 'b',  99,     0,  32,   0,   0,   0,     0,    0}  // 32 OxyLow
+	  ,{"CPAll", "1619", 'b',  13,     0,  0,   0,   0,   0,     0,    0}   // All Caution Lights on, index 7!
 /* //  DED
 	  ,{"DED1",   "0231", 's',  79,   0,  "",  ".1"}          //Variable 2 - DED Line 1 index 40
 	  ,{"DED2",   "0232", 's',  79,   0,  "",  ".2"}          //Variable 3 - DED Line 2
 	  ,{"DED3",   "0233", 's',  79,   0,  "",  ".3"}          //Variable 4 - DED Line 3
 	  ,{"DED4",   "0234", 's',  79,   0,  "",  ".4"}          //Variable 5 - DED Line 4
- 	  ,{"DED5",   "0235", 's',  79,   0,  "",  ".5"}          //Variable 6 - DED Line 5  
-   }; 
+ 	  ,{"DED5",   "0235", 's',  79,   0,  "",  ".5"}          //Variable 6 - DED Line 5
+   };
   const byte variableCount = sizeof(datenfeld)/sizeof(datenfeld[0]); */
--- Arduino/SimController/SimController.ino
+++ Arduino/SimController/SimController.ino
@@ -10,69 +10,69 @@ modIndex: if more modules of same hardware are used, they will be configured in 
 valIndex: can be used as single bit value to change in a LED module... let's see, how we're going to use it :-)
 Type: one of CHAR, INT, LONG, STRING
 */
-struct f16var {  
+struct f16var {
     unsigned char number;
     unsigned char module;
     unsigned char modIndex;
     unsigned char valIndex;
     //unsigned char type;
 
-	enum Type {	
+	enum Type {
 		CHAR =    0,
-    INT =     1,  
-    LONG =    2,  
-    STRING =  3  
-  } type; 
+    INT =     1,
+    LONG =    2,
+    STRING =  3
+  } type;
 
 	 // Union for different data types
-    union Value {  
+    union Value {
       unsigned char valC;
-      unsigned int valI;  
-      unsigned long valL;  
-      String* valString;  
+      unsigned int valI;
+      unsigned long valL;
+      String* valString;
 
     } value;
-	
+
     // constructor
     f16var(unsigned char number, unsigned char module, unsigned char modIndex, unsigned char valIndex, Type type) : number(number), module(module), modIndex(modIndex), valIndex(valIndex), type(type) {  }
-      
-    
-}; 
+
+
+};
 
 struct f16varC : public f16var {
 //  unsigned char value;
-    
+
   f16varC(unsigned char number, unsigned char module, unsigned char modIndex, unsigned char valIndex, unsigned char value) : f16var(number, module, modIndex, valIndex, CHAR) {
 	  this->value.valC = value;
 	}
 };
 
 struct f16varI : public f16var {  // using I as in integer for Short, since both are 2 bytes on arduino
-  
+
   f16varI(unsigned char number, unsigned char module, unsigned char modIndex, unsigned char valIndex, unsigned char value) : f16var(number, module, modIndex, valIndex, INT)  {
-	  
+
 	  this->value.valI = value;
-	  
+
   }
 };
 
 struct f16varL : public f16var {
-  
+
   f16varL(unsigned char number, unsigned char module, unsigned char modIndex, unsigned char valIndex, unsigned char value) : f16var(number, module, modIndex, valIndex, LONG) {
-	  this->value.valL = value;	  
+	  this->value.valL = value;
   }
 };
 
 struct f16varS : public f16var {
-  
+
   f16varS(unsigned char number, unsigned char module, unsigned char modIndex, unsigned char valIndex, unsigned char value) : f16var(number, module, modIndex, valIndex, STRING) {
 	  this->value.valString = new String(value);
   }
-  
-      // Destructor  
-    ~f16varS() {  
-        delete value.valString; 
-    }  
+
+      // Destructor
+    ~f16varS() {
+        delete value.valString;
+    }
 };
 
 //////////////////////////////
@@ -122,47 +122,48 @@ unsigned long gap = 0;
 	#include "SC_SSegMAX7219.h"
 #endif
  #ifdef ServoMotor
-  #include "SC_Servo.h"    
+  #include "SC_Servo.h"
  #endif
  #ifdef StepperX27
-  #include "SC_StepperX27.h"    
+  #include "SC_StepperX27.h"
  #endif
 //Steppermotor ControllerBoard settings
-#ifdef StepperVID                   
-  #include "SC_StepperVID.h"
-#endif  
+#ifdef StepperVID
+  //  #include "SC_StepperVID.h"
+  #include "SC_StepperAX120.h"
+#endif
 
  //7Segment settings TM1637 tube
-#ifdef SSegTM1637                            
+#ifdef SSegTM1637
   #include "SC_SSegTM1637.h"
-#endif  
+#endif
 //PWM Shield for Servos settings
-#ifdef ServoPWM                       
-  #include "SC_ServoPWMShield.h"               
-#endif  
+#ifdef ServoPWM
+  #include "SC_ServoPWMShield.h"
+#endif
 //Steppermotor settings
-#ifdef StepperBYJ                  
+#ifdef StepperBYJ
   #include "SC_Stepper28BYJ48.h"
-#endif                                
+#endif
 
 //Compass setting
-#ifdef CompassX27                  
+#ifdef CompassX27
   #include "SC_CompassX27.h"
-#endif  
+#endif
 
 //Altimeter setting
-#ifdef AltimeterX27                  
+#ifdef AltimeterX27
   #include "SC_AltimeterX27.h"
   #include "AltimeterInternal.h"
-#endif  
+#endif
 //motor poti settings
-#ifdef MotorPoti 
+#ifdef MotorPoti
   #include "SC_MotorPoti.h"
-#endif 
+#endif
 #ifdef OLED
   #include "SC_OLED.h"
 #endif
-#ifdef SpeedBrake                             
+#ifdef SpeedBrake
   #include "SC_SBI.h"
 #endif
 #ifdef FuelFlowIndicator
@@ -170,12 +171,12 @@ unsigned long gap = 0;
 #endif
 
 //rotary encoders
-#ifdef RotEncoder               
+#ifdef RotEncoder
   #include "SC_Encoder.h"
-#endif  
+#endif
 
 
- 
+
 
 //////////////////////////////
 // function imlementations
@@ -208,8 +209,8 @@ void setupModules() {
    SetupTM1637();
   #endif                                //7-segment-display TM1637 setup end
   #ifdef ServoPWM                       //servo PWM shield setup begin
-    SetupServoPWM();          
-  #endif                                //servo PWM shield setup end 
+    SetupServoPWM();
+  #endif                                //servo PWM shield setup end
   #ifdef StepperBYJ                     //stepper setup begin
    //SetupStepperBYJ();
   #endif                                //stepper setup end
@@ -238,52 +239,52 @@ void setupModules() {
   #ifdef RotEncoder                     //Rotary encoder setup begin
    SetupEncoder();
   #endif                                //Rotary encoder setup end
-  
+
 }
 
 ///Call for another update of the motors to allow for fast, fluent movement
 void fastUpdate()
 {
-  #ifdef ServoMotor                       
-    Servo_FastUpdate();            
-  #endif                            
+  #ifdef ServoMotor
+    Servo_FastUpdate();
+  #endif
 
-  #ifdef ServoPWM                       
-    ServoPWM_FastUpdate();              
-  #endif 
+  #ifdef ServoPWM
+    ServoPWM_FastUpdate();
+  #endif
 
   #ifdef StepperVID
-    StepperVID_FastUpdate();   
+    StepperVID_FastUpdate();
   #endif
-   
+
   #ifdef StepperX27
-    StepperX27_FastUpdate();  
+    StepperX27_FastUpdate();
   #endif
-   
+
   #ifdef Stepper28BYJ48
-    Stepper28BYJ48_FastUpdate();  
-  #endif 
+    Stepper28BYJ48_FastUpdate();
+  #endif
 
   #ifdef CompassX27
     CompassX27_FastUpdate();
-  #endif      
+  #endif
 
   #ifdef AltimeterX27
     AltimeterX27_FastUpdate();
-  #endif      
+  #endif
 
   #ifdef MotorPoti
-    MotorPoti_FastUpdate(); 
-  #endif  
+    MotorPoti_FastUpdate();
+  #endif
 }
 
 
 void outputVars() {
-	
-  int i = lastParsedVar;  
+
+  int i = lastParsedVar;
   switch (vars[i]->module) {
     #ifdef LED_MM5451
-    case MODMM5451:            
+    case MODMM5451:
       UpdateLED_MM5451(i);
       break;
     #endif
@@ -302,12 +303,12 @@ void outputVars() {
       UpdateStepperX27(i);
       break;
     #endif
-    #ifdef DED_PFL  
+    #ifdef DED_PFL
     case MODDED:
       if (millis() > lastDEDUpdate + DEDUPATEINTERVALL) {
-          UpdateDED();  
+          UpdateDED();
           lastDEDUpdate = millis();
-      }  
+      }
       break;
     #endif
     #ifdef SSegTM1637
@@ -315,21 +316,21 @@ void outputVars() {
       UpdateTM1637(i);
       break;
     #endif
-    #ifdef ServoPWM  
+    #ifdef ServoPWM
       case MODSERVOPWM: //Servo on PWM Shield
         UpdateServoPWM(i);
         break;
-    #endif    
-    #ifdef StepperBYJ 
+    #endif
+    #ifdef StepperBYJ
       case MODSTEPBYJ: //Stepper Motor
         UpdateStepperBYJ(i);
         break;
-    #endif 
-    #ifdef HDSP253X 
+    #endif
+    #ifdef HDSP253X
       case MODHDSP253X: //Stepper Motor
         UpdateHDSP253X(i);
         break;
-    #endif 
+    #endif
     #ifdef StepperVID
       case MODX27VID: //one to four Servos on a VID6606 board
         UpdateStepperVID(i);
@@ -339,7 +340,7 @@ void outputVars() {
       case MODCOMPASS: //compass driven by X-class stepper motor
         UpdateCompassX27(i);
         break;
-    #endif	  
+    #endif
 	  #ifdef AltimeterX27
       case MODALTIMETER: //altimeter needle driven by X-class stepper motor
         UpdateAltimeterX27(i);
@@ -348,45 +349,45 @@ void outputVars() {
     #ifdef MotorPoti
       case MODMOTORPOTI: //MotorPoti
         UpdateMotorPoti(i);
-        break;      
+        break;
     #endif
 
     #ifdef OLED
       case MODOLED: //generic OLED
         UpdateOLED(i);
-        break;      
+        break;
     #endif
-    
+
     #ifdef SpeedBrake
       case MODSPEEDBRK: //Speedbrake OLED
         UpdateSBI(i);
-        break;      
+        break;
     #endif
-    
+
     #ifdef FuelFlowIndicator
       case MODFFI: //FFI OLED
         UpdateFFI(i);
-        break;      
+        break;
     #endif
-    
-    
-    
+
+
+
   }
   varsChanged = false;
-      
+
 }
 
 void UpdateInput(bool all)
 {
-  
-  #ifdef RotEncoder              
+
+  #ifdef RotEncoder
    CheckEncoder();            //check encoders and initiate commands if they were moved
   #endif
-  
+
   #ifdef AltimeterX27
 	UpdateAltimeterKnob();
   #endif
- 
+
 }
 
 //////////////////////////////
@@ -413,9 +414,9 @@ void loop() {
   // showNewData();
   parseSerialCommand();
   fastUpdate();
-  if (varsChanged) outputVars();	
+  if (varsChanged) outputVars();
   fastUpdate();
   UpdateInput(false);
   delay(5);
-  
+
 }
